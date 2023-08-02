07b83ec177b07d5913ce10109e67a64cd17d3aa3
AhnYeonghoo <dksdudgn00@naver.com>
2023-08-03 06:30:40 +0900

    시간복잡도

---
+++ 시간복잡도.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int mainTest()
+{
+    int findNumber;
+    srand(time(NULL));
+    findNumber = rand() & 100;
+    
+    for (int i = 0; i < 100; i++) {
+        if (i == findNumber) {
+            cout << i;
+            break;
+        }
+    }
+
+    return 0;
+}
